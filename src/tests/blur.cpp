bde95a01a3923f99dbd4a183331a17dd31f3b1e9
Gaetan Guidet <gatgui@gmail.com>
2010-05-02 14:30:49 +0900

    Updated standard blur

--- src/tests/blur.cpp
+++ src/tests/blur.cpp
@@ -74,7 +74,7 @@ class BlurEffect : public ofx::ImageEffect {
     
     ofx::ChoiceParameter pType;
     ofx::ChoiceParameter pFilter;
-    ofx::IntParameter pSamples;
+    ofx::Int2Parameter pRadius;
     ofx::DoubleParameter pZoom;
     ofx::DoubleParameter pAngle;
     ofx::Double2Parameter pCenter;
@@ -131,6 +131,7 @@ OfxStatus BlurDescriptor::describeInContext(ofx::ImageEffectContext ctx) {
   ofx::ClipDescriptor mClip = defineClip("Mask");
   mClip.setSupportedComponent(0, ofx::ImageComponentRGBA);
   mClip.setSupportedComponent(0, ofx::ImageComponentAlpha);
+  mClip.setOptional(true);
   
   ofx::ChoiceParameterDescriptor type = parameters().defineChoiceParam("type");
   type.setAnimating(false);
@@ -144,15 +145,19 @@ OfxStatus BlurDescriptor::describeInContext(ofx::ImageEffectContext ctx) {
   filter.setAnimating(false);
   filter.setPersistant(true);
   filter.setChoice(0, "box");
-  filter.setChoice(1, "triangular");
+  filter.setChoice(1, "triangle");
   filter.setChoice(2, "gaussian");
   filter.setDefault(2);
   
-  ofx::IntParameterDescriptor samples = parameters().defineIntParam("samples");
-  samples.setAnimating(true);
-  samples.setPersistant(true);
-  samples.setMin(0);
-  samples.setMax(1024);
+  ofx::Int2ParameterDescriptor radius = parameters().defineInt2Param("radius");
+  radius.setAnimating(false);
+  radius.setPersistant(true);
+  radius.setMin(0, 0);
+  radius.setMax(100, 100);
+  radius.setDefault(2, 2);
+  // this seems to fail
+  //radius.setDimensionLabel(0, "dir1");
+  //radius.setDimensionLabel(1, "dir2");
   
   // use for directional and radial blur
   ofx::DoubleParameterDescriptor angle = parameters().defineDoubleParam("angle");
@@ -163,12 +168,11 @@ OfxStatus BlurDescriptor::describeInContext(ofx::ImageEffectContext ctx) {
   angle.setMax(360);
   angle.setIncrement(0.001);
   angle.setDigits(3);
+  angle.enable(false);
   
   // used for radial blur only
   ofx::Double2ParameterDescriptor center = parameters().defineDouble2Param("center");
   center.setMin(0, 0);
-  center.setMin(1, 0);
-  center.setMax(0, 1);
   center.setMax(1, 1);
   center.setDefault(0.5, 0.5);
   center.setPersistant(true);
@@ -178,6 +182,7 @@ OfxStatus BlurDescriptor::describeInContext(ofx::ImageEffectContext ctx) {
   center.setDigits(3);
   center.setIncrement(0.001);
   center.setDoubleType(ofx::DoubleParamNormalisedXY);
+  center.enable(false);
   
   // used for radial blur only
   ofx::DoubleParameterDescriptor zoom = parameters().defineDoubleParam("zoom");
@@ -188,6 +193,7 @@ OfxStatus BlurDescriptor::describeInContext(ofx::ImageEffectContext ctx) {
   zoom.setDefault(0.001);
   zoom.setDigits(3);
   zoom.setIncrement(0.001);
+  zoom.enable(false);
   
   return kOfxStatOK;
 }
@@ -201,7 +207,7 @@ BlurEffect::BlurEffect(ofx::ImageEffectHost *h, OfxImageEffectHandle hdl)
   cMask = getClip("Mask");
   pType = parameters().getChoiceParam("type");
   pFilter = parameters().getChoiceParam("filter");
-  pSamples = parameters().getIntParam("samples");
+  pRadius = parameters().getInt2Param("radius");
   pAngle = parameters().getDoubleParam("angle");
   pZoom = parameters().getDoubleParam("zoom");
   pCenter = parameters().getDouble2Param("center");
@@ -211,8 +217,10 @@ BlurEffect::~BlurEffect() {
 }
 
 OfxStatus BlurEffect::isIdentity(ofx::ImageEffect::IsIdentityArgs &args) {
-  ofx::IntParameter samples = parameters().getIntParam("samples");
-  if (samples.getValue() == 0) {
+  int wsamples=0, hsamples=0;
+  // this seems to fail... wsamples seems to be always 0
+  pRadius.getValue(wsamples, hsamples);
+  if (wsamples == 0 && hsamples == 0) {
     args.idClip = "Source";
     args.idTime = args.time;
     return kOfxStatOK;
@@ -228,70 +236,60 @@ OfxStatus BlurEffect::render(ofx::ImageEffect::RenderArgs &args) {
   
   ofx::RGBAColourF *srcPix, *dstPix;
   
-  // filter width = nsamples*2 + 1 [center pix]
-  int nsamples = pSamples.getValueAtTime(args.time);
-  float *weights = new float[nsamples + 1];
+  // filter width = wsamples*2 + 1
+  int wsamples=0, hsamples=0;
+  
+  pRadius.getValue(wsamples, hsamples);
+  
+  float *wweights = new float[wsamples + 1];
+  float *hweights = new float[hsamples + 1];
   
   if (pFilter.getValue() == 0) {
     
-    float sw = 1.0f / float(2 * nsamples + 1);
-    
-    for (int i=0; i<=nsamples; ++i) {
-      weights[i] = sw;
+    for (int i=0; i<=wsamples; ++i) {
+      wweights[i] = 1.0f;
+    }
+    for (int i=0; i<=hsamples; ++i) {
+      hweights[i] = 1.0f;
     }
     
   } else if (pFilter.getValue() == 1) {
     
-    float isum = 0.0f;
-    float sw, fw = float(nsamples) + 1.0f;
-    
-    weights[0] = fw;
-    
-    for (int i=1; i<=nsamples; ++i) {
-      sw = fw - float(i);
-      weights[i] = sw;
-      isum += sw;
+    for (int i=0; i<=wsamples; ++i) {
+      wweights[i] = float(wsamples + 1 - i);
     }
-    
-    isum = 1.0f / (2.0f * isum + fw);
-    
-    for (int i=0; i<=nsamples; ++i) {
-      weights[i] *= isum;
+    for (int i=0; i<=hsamples; ++i) {
+      hweights[i] = float(hsamples + 1 - i);
     }
     
   } else {
     
-    float isum = 0.0f;
-    float sw;
-    float i2ss = 1.0f / (2.0f * nsamples); // inverse 2 sigma square
+    float wtheta = float((wsamples + 1) / 3);
+    float htheta = float((hsamples + 1) / 3);
     
-    weights[0] = 1.0f;
-    for (int i=1; i<=nsamples; ++i) {
-      sw = exp(- float(i) * float(i) * i2ss);
-      weights[i] = sw;
-      isum += sw;
+    float wInv2ThetaSqr = 1.0f / (2.0f * wtheta * wtheta);
+    float hInv2ThetaSqr = 1.0f / (2.0f * htheta * htheta);
+    
+    for (int i=0; i<=wsamples; ++i) {
+      wweights[i] = (float) exp(- wInv2ThetaSqr * i * i);
     }
-    
-    isum = 1.0f / (2.0f * isum + 1.0f);
-    
-    for (int i=0; i<=nsamples; ++i) {
-      weights[i] *= isum;
+    for (int i=0; i<=hsamples; ++i) {
+      hweights[i] = (float) exp(- hInv2ThetaSqr * i * i);
     }
-    
   }
   
   if (pType.getValue() == 0) {
     // standard blur
-    ofx::Log("gatgui.filter.multiBlur: standard %d samples", nsamples);
+    ofx::Log("gatgui.filter.multiBlur: standard, %dx%d samples", wsamples, hsamples);
+    
+    // renderScale ?
     
     int tmpw = args.renderWindow.x2 - args.renderWindow.x1;
     int tmph = args.renderWindow.y2 - args.renderWindow.y1;
     int pixSize = 4 * sizeof(float);
     int rowSize = tmpw * pixSize;
     
-    OfxImageMemoryHandle hTmp = alloc(tmph * rowSize);
-    
-    unsigned char *tmpPix = (unsigned char*) lock(hTmp);
+    unsigned char *tmpPix = new unsigned char[tmpw * tmph * pixSize];
     dstPix = (ofx::RGBAColourF*) tmpPix;
     
     // first pass (horizontal)
@@ -304,19 +302,24 @@ OfxStatus BlurEffect::render(ofx::ImageEffect::RenderArgs &args) {
         dstPix->g = 0.0f;
         dstPix->b = 0.0f;
         dstPix->a = 0.0f;
-        for (int x2=x-nsamples; x2<=x+nsamples; ++x2) {
-          int widx = x2 - x;
+        float w = 0.0f;
+        int widx = 0;
+        for (int x2=x-wsamples; x2<=x+wsamples; ++x2) {
+          widx = x2 - x;
           widx = (widx < 0 ? -widx : widx);
-          if (!iSource.pixelAddress(x2, y, srcPix)) {
-            //assume black opaque pixel
-            dstPix->a += weights[widx];
-          } else {
-            dstPix->r += weights[widx] * srcPix->r;
-            dstPix->g += weights[widx] * srcPix->g;
-            dstPix->b += weights[widx] * srcPix->b;
-            dstPix->a += weights[widx] * srcPix->a;
+          if (iSource.pixelAddress(x2, y, srcPix)) {
+            w += wweights[widx];
+            dstPix->r += wweights[widx] * srcPix->r;
+            dstPix->g += wweights[widx] * srcPix->g;
+            dstPix->b += wweights[widx] * srcPix->b;
+            dstPix->a += wweights[widx] * srcPix->a;
           }
         }
+        w = 1.0f / w;
+        dstPix->r *= w;
+        dstPix->g *= w;
+        dstPix->b *= w;
+        dstPix->a *= w;
         ++dstPix;
       }
     }
@@ -331,26 +334,31 @@ OfxStatus BlurEffect::render(ofx::ImageEffect::RenderArgs &args) {
           continue;
         }
         srcPix = (ofx::RGBAColourF*)(tmpPix +
-                                     ((y - nsamples) - args.renderWindow.y1) * rowSize +
+                                     ((y - hsamples) - args.renderWindow.y1) * rowSize +
                                      (x - args.renderWindow.x1) * pixSize);
-        for (int y2=y-nsamples; y2<=y+nsamples; ++y2) {
-          int widx = y2 - y;
+        float w = 0.0f;
+        int widx = 0;
+        for (int y2=y-hsamples; y2<=y+hsamples; ++y2) {
+          widx = y2 - y;
           widx = (widx < 0 ? -widx : widx);
-          if (y2 < args.renderWindow.y1 || y2 >= args.renderWindow.y2) {
-            dstPix->a += weights[widx];
-          } else {
-            dstPix->r += weights[widx] * srcPix->r;
-            dstPix->g += weights[widx] * srcPix->g;
-            dstPix->b += weights[widx] * srcPix->b;
-            dstPix->a += weights[widx] * srcPix->a;
+          if (y2 >= args.renderWindow.y1 && y2 < args.renderWindow.y2) {
+            w += hweights[widx];
+            dstPix->r += hweights[widx] * srcPix->r;
+            dstPix->g += hweights[widx] * srcPix->g;
+            dstPix->b += hweights[widx] * srcPix->b;
+            dstPix->a += hweights[widx] * srcPix->a;
           }
           srcPix += tmpw;
         }
+        w = 1.0f / w;
+        dstPix->r *= w;
+        dstPix->g *= w;
+        dstPix->b *= w;
+        dstPix->a *= w;
       }
     }
     
-    unlock(hTmp);
-    free(hTmp);
+    delete[] tmpPix;
     
   } else {
     
@@ -378,7 +386,8 @@ OfxStatus BlurEffect::render(ofx::ImageEffect::RenderArgs &args) {
     }
   }
   
-  delete[] weights;
+  delete[] wweights;
+  delete[] hweights;
   
   // mask?
   

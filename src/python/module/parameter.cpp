172262b57ff4fac33e678e3664ab9b5dc4253cce
Gaetan Guidet <gatgui@gmail.com>
2010-08-08 13:41:37 +0900

    completed binding for ofx::ValueParameter class

--- src/python/module/parameter.cpp
+++ src/python/module/parameter.cpp
@@ -2237,7 +2237,669 @@ static PyGetSetDef PyOFXParameter_GetSeters[] =
 
 // ---
 
+int PyOFXValueParameter_Init(PyObject *, PyObject *, PyObject *)
+{
+  return 0;
+}
 
+PyObject* PyOFXValueParameter_GetInteract(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  //ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+PyObject* PyOFXValueParameter_GetInteractSize(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  double w, h;
+  
+  CATCH({param->getInteractSize(w, h);}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  PyObject *rv = PyTuple_New(2);
+  PyTuple_SetItem(rv, 0, PyFloat_FromDouble(w));
+  PyTuple_SetItem(rv, 1, PyFloat_FromDouble(h));
+  return rv;
+}
+
+PyObject* PyOFXValueParameter_GetInteractSizeAspect(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  double rv;
+  
+  CATCH({rv = param->getInteractSizeAspect();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  return PyFloat_FromDouble(rv);
+}
+
+PyObject* PyOFXValueParameter_GetInteractMinSize(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  int w, h;
+  
+  CATCH({param->getInteractMinSize(w, h);}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  PyObject *rv = PyTuple_New(2);
+  PyTuple_SetItem(rv, 0, PyInt_FromLong(w));
+  PyTuple_SetItem(rv, 1, PyInt_FromLong(h));
+  return rv;
+}
+
+PyObject* PyOFXValueParameter_GetInteractPreferedSize(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  int w, h;
+  
+  CATCH({param->getInteractPreferedSize(w, h);}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  PyObject *rv = PyTuple_New(2);
+  PyTuple_SetItem(rv, 0, PyInt_FromLong(w));
+  PyTuple_SetItem(rv, 1, PyInt_FromLong(h));
+  return rv;
+}
+
+PyObject* PyOFXValueParameter_GetAnimateable(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->isAnimateable();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+PyObject* PyOFXValueParameter_GetAnimating(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->isAnimating();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+PyObject* PyOFXValueParameter_GetAutoKeying(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->isAutoKeying();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+PyObject* PyOFXValueParameter_GetPersistant(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->isPersistant();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+PyObject* PyOFXValueParameter_GetEvaluateOnChange(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->evaluateOnChange();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+PyObject* PyOFXValueParameter_GetPluginMayWrite(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->mayPluginWrite();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+PyObject* PyOFXValueParameter_GetCacheInvalidation(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  ofx::ParamInvalidate rv;
+  
+  CATCH({rv = param->getCacheInvalidation();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  return PyInt_FromLong(long(rv));
+}
+
+#ifdef OFX_API_1_2
+
+PyObject* PyOFXValueParameter_GetUseHostOverlayHandle(PyObject *self, void*)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->useHostOverlayHandle();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+#endif
+
+static PyGetSetDef PyOFXValueParameter_GetSeters[] =
+{
+  {(char*)"interact", PyOFXValueParameter_GetInteract, NULL, NULL, NULL},
+  {(char*)"interactSize", PyOFXValueParameter_GetInteractSize, NULL, NULL, NULL},
+  {(char*)"interactSizeAspect", PyOFXValueParameter_GetInteractSizeAspect, NULL, NULL, NULL},
+  {(char*)"interactMinSize", PyOFXValueParameter_GetInteractMinSize, NULL, NULL, NULL},
+  {(char*)"interactPreferedSize", PyOFXValueParameter_GetInteractPreferedSize, NULL, NULL, NULL},
+  {(char*)"animateable", PyOFXValueParameter_GetAnimateable, NULL, NULL, NULL},
+  {(char*)"animating", PyOFXValueParameter_GetAnimating, NULL, NULL, NULL},
+  {(char*)"autoKeying", PyOFXValueParameter_GetAutoKeying, NULL, NULL, NULL},
+  {(char*)"persistant", PyOFXValueParameter_GetPersistant, NULL, NULL, NULL},
+  {(char*)"evaluateOnChange", PyOFXValueParameter_GetEvaluateOnChange, NULL, NULL, NULL},
+  {(char*)"pluginMayWrite", PyOFXValueParameter_GetPluginMayWrite, NULL, NULL, NULL},
+  {(char*)"cacheInvalidation", PyOFXValueParameter_GetCacheInvalidation, NULL, NULL, NULL},
+#ifdef OFX_API_1_2
+  {(char*)"useHostOverlayHandle", PyOFXValueParameter_GetUseHostOverlayHandle, NULL, NULL, NULL},
+#endif
+  {NULL, NULL, NULL, NULL, NULL}
+};
+
+PyObject* PyOFXValueParameter_GetNumKeys(PyObject *self, PyObject *)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  unsigned int rv;
+  
+  CATCH({rv = param->getNumKeys();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  return PyInt_FromLong(long(rv));
+}
+
+PyObject* PyOFXValueParameter_GetKeyTime(PyObject *self, PyObject *args)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  unsigned int nth;
+  
+  if (!PyArg_ParseTuple(args, "I", &nth))
+  {
+    return NULL;
+  }
+  
+  bool failed = false;
+  
+  ofx::Time rv;
+  
+  CATCH({rv = param->getKeyTime(nth);}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  return PyFloat_FromDouble(rv);
+}
+
+PyObject* PyOFXValueParameter_GetKeyIndex(PyObject *self, PyObject *args)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  double t;
+  int d = int(ofx::KeyDirectionExact);
+  
+  if (!PyArg_ParseTuple(args, "d|i", &t, &d))
+  {
+    return NULL;
+  }
+  
+  bool failed = false;
+  
+  int rv;
+  
+  CATCH({rv = param->getKeyIndex(t, ofx::KeyDirection(d));}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  return PyInt_FromLong(rv);
+}
+
+PyObject* PyOFXValueParameter_DeleteKey(PyObject *self, PyObject *args)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  double t;
+  
+  if (!PyArg_ParseTuple(args, "d", &t))
+  {
+    return NULL;
+  }
+  
+  bool failed = false;
+  
+  bool rv;
+  
+  CATCH({rv = param->deleteKey(t);}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  if (rv)
+  {
+    Py_INCREF(Py_True);
+    return Py_True;
+  }
+  else
+  {
+    Py_INCREF(Py_False);
+    return Py_False;
+  }
+}
+
+PyObject* PyOFXValueParameter_DeleteAllKeys(PyObject *self, PyObject *)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  bool failed = false;
+  
+  CATCH({param->deleteAllKeys();}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+PyObject* PyOFXValueParameter_Copy(PyObject *self, PyObject *args)
+{
+  PyOFXValueParameter *pparam = (PyOFXValueParameter*) self;
+  
+  if (!pparam->base.param)
+  {
+    PyErr_SetString(PyExc_RuntimeError, "Unbound object");
+    return NULL;
+  }
+  
+  ofx::ValueParameter *param = (ofx::ValueParameter*) pparam->base.param;
+  
+  PyObject *pfrom = 0;
+  double offset = 0.0;
+  PyObject *prange = 0;
+  
+  if (!PyArg_ParseTuple(args, "Od|O", &pfrom, &offset, &prange))
+  {
+    
+  }
+  
+  ofx::ValueParameter *from = 0;
+  
+  if (!PyObject_TypeCheck(pfrom, &PyOFXValueParameterType))
+  {
+    PyErr_SetString(PyExc_TypeError, "Expected a ofx.ValueParameter object");
+    return NULL;
+  }
+  
+  from = (ofx::ValueParameter*) (((PyOFXValueParameter*) pfrom)->base.param);
+  
+  ofx::FrameRange *range = 0;
+  ofx::FrameRange _range;
+  
+  if (prange)
+  {
+    if (!PyTuple_Check(prange))
+    {
+      PyErr_SetString(PyExc_TypeError, "Expected a tuple");
+      return NULL;
+    }
+    if (PyTuple_Size(prange) != 2)
+    {
+      PyErr_SetString(PyExc_ValueError, "Expected a tuple of two values");
+      return NULL;
+    }
+    range = &_range;
+    range->first = PyFloat_AsDouble(PyTuple_GetItem(prange, 0));
+    range->second = PyFloat_AsDouble(PyTuple_GetItem(prange, 1));
+  }
+  
+  bool failed = false;
+  
+  CATCH({param->copy(*from, offset, range);}, failed);
+  
+  if (failed)
+  {
+    return NULL;
+  }
+  
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+static PyMethodDef PyOFXValueParameter_Methods[] =
+{
+  {"getNumKeys", PyOFXValueParameter_GetNumKeys, METH_VARARGS, NULL},
+  {"getKeyTime", PyOFXValueParameter_GetKeyTime, METH_VARARGS, NULL},
+  {"getKeyIndex", PyOFXValueParameter_GetKeyIndex, METH_VARARGS, NULL},
+  {"deleteKey", PyOFXValueParameter_DeleteKey, METH_VARARGS, NULL},
+  {"deleteAllKeys", PyOFXValueParameter_DeleteAllKeys, METH_VARARGS, NULL},
+  {"copy", PyOFXValueParameter_Copy, METH_VARARGS, NULL},
+  {NULL, NULL, NULL, NULL}
+};
 
 // ---
 
@@ -2275,6 +2937,17 @@ bool PyOFX_InitParameter(PyObject *mod)
     return false;
   }
   
+  INIT_TYPE(PyOFXValueParameterType, "ofx.ValueParameter", PyOFXValueParameter);
+  PyOFXValueParameterType.tp_base = &PyOFXParameterType;
+  PyOFXValueParameterType.tp_flags = Py_TPFLAGS_DEFAULT|Py_TPFLAGS_BASETYPE;
+  PyOFXValueParameterType.tp_init = PyOFXValueParameter_Init;
+  PyOFXValueParameterType.tp_getset = PyOFXValueParameter_GetSeters;
+  PyOFXValueParameterType.tp_methods = PyOFXValueParameter_Methods;
+  if (PyType_Ready(&PyOFXValueParameterType) < 0)
+  {
+    return false;
+  }
+  
   // add new types to module
   
   Py_INCREF(&PyOFXParameterDescriptorType);
@@ -2285,6 +2958,9 @@ bool PyOFX_InitParameter(PyObject *mod)
   
   Py_INCREF(&PyOFXParameterType);
   PyModule_AddObject(mod, "Parameter", (PyObject*)&PyOFXParameterType);
+  
+  Py_INCREF(&PyOFXValueParameterType);
+  PyModule_AddObject(mod, "ValueParameter", (PyObject*)&PyOFXValueParameterType);
   
   return true;
 }
